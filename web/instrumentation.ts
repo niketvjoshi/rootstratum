5cd2db375a0f82b6a6ad79ca883c48555d5f164f
Niket Joshi <niketjoshi2006@gmail.com>
2026-08-09 20:41:29 +0530

    set OTel service name to rootstratum-website

--- web/instrumentation.ts
+++ web/instrumentation.ts
@@ -2,8 +2,10 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { NodeSDK } = await import('@opentelemetry/sdk-node')
     const { OTLPTraceExporter } = await import('@opentelemetry/exporter-trace-otlp-http')
+    const { Resource } = await import('@opentelemetry/resources')
 
     const sdk = new NodeSDK({
+      resource: new Resource({ 'service.name': 'rootstratum-website' }),
       traceExporter: new OTLPTraceExporter({
         url: 'https://otlp.nr-data.net:4318/v1/traces',
         headers: {
